a1c7f6470c0553aa26d6621d337042b3f517735e
MrCyci6 <nozuriafaction@gmail.com>
2024-06-23 21:46:06 +0200

    Add files via upload

--- js/type.js
+++ js/type.js
@@ -1,31 +1,38 @@
-const phrase = "Votre imagination n'a plus de limites.";
+const phrases = [
+    "Votre imagination n'a plus de limites.",
+    "Transformons vos idées en solutions concrètes.",
+    "Alliez performance et innovation."
+];
+
+let currentPhraseIndex = 0;
 let currentLetterIndex = 0;
 const typingSpeed = 50;
-const erasingSpeed = 50;
-const delayBetweenActions = 2000;
+const erasingSpeed = 25;
+const delayBetweenPhrases = 1000;
 
 const animatedTextElement = document.querySelector('.animated-text');
 
 function typePhrase() {
-    if (currentLetterIndex < phrase.length) {
-        animatedTextElement.textContent += phrase.charAt(currentLetterIndex);
+    if (currentLetterIndex < phrases[currentPhraseIndex].length) {
+        animatedTextElement.textContent += phrases[currentPhraseIndex].charAt(currentLetterIndex);
         currentLetterIndex++;
         setTimeout(typePhrase, typingSpeed);
     } else {
-        setTimeout(erasePhrase, delayBetweenActions);
+        setTimeout(erasePhrase, delayBetweenPhrases);
     }
 }
 
 function erasePhrase() {
     if (currentLetterIndex > 0) {
-        animatedTextElement.textContent = phrase.substring(0, currentLetterIndex - 1);
+        animatedTextElement.textContent = phrases[currentPhraseIndex].substring(0, currentLetterIndex - 1);
         currentLetterIndex--;
         setTimeout(erasePhrase, erasingSpeed);
     } else {
-        setTimeout(typePhrase, delayBetweenActions);
+        currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+        setTimeout(typePhrase, typingSpeed);
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(typePhrase, delayBetweenActions);
+    setTimeout(typePhrase, delayBetweenPhrases);
 });
